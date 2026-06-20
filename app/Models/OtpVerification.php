e607bda17e37e4af42d24a88fe9856bfb56af67c
Huzaifa <huzaifakhokhar413@email.com>
2026-06-20 13:22:57 +0500

    Added fillable property to OTP model

--- app/Models/OtpVerification.php
+++ app/Models/OtpVerification.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OtpVerification extends Model
 {
-    //
+    use HasFactory;
+
+    // 🚀 Yeh line add karein taake Laravel data save karne de
+    protected $fillable = ['email', 'otp'];
 }
